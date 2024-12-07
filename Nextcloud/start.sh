f58f34cbb2825af0c0a8bf61c4e71f52d2db8f88
RubenArtz <49426855+RubenArtz@users.noreply.github.com>
2024-12-07 09:02:23 -0600

    Delete Nextcloud directory

--- Nextcloud/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ash
-rm -rf /home/container/tmp/*
-echo "⚙️ Script Version: 1.8"
-echo "🛠 Starting PHP-FPM..."
-/usr/sbin/php-fpm81 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
-echo "🛠 Starting Nginx..."
-echo "✅ Successfully started"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
